8e8d4bc6c152d2077c68066b35d44822658f5819
Jiyan He <volltin@live.com>
2018-10-16 16:18:54 +0800

    Add engine

---
+++ biohub/search/engine/engine.py
@@ -0,0 +1,43 @@
+from .parser import PeopleParser, TimeParser, LabelParser
+
+def test():
+    s = "reports by some_people from 2017/01/23 to 2018/10/28 #some_label"
+    time_parser = TimeParser(s)
+    people_parser = PeopleParser(s)
+    label_parser = LabelParser(s)
+    filters = {
+        "time": time_parser.parse(),
+        "people": people_parser.parse(),
+        "label": label_parser.parse()
+    }
+
+    data = {
+        "users" : [],
+        "reports" : [],
+    }
+
+    ranks = {
+        "users": 1,
+        "reports": 2,
+        "db": -1,
+    }
+
+    return {
+        "filters": filters,
+        "ranks": ranks,
+        "data": data
+    }
+
+
+class Engine:
+    def __init__(self):
+        pass
+
+    def test(self):
+        return test()
+
+def main():
+    print(test())
+
+if __name__ == "__main__":
+    main()
--- biohub/search/views.py
+++ biohub/search/views.py
@@ -4,8 +4,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import viewsets, mixins, decorators
 # Create your views here.
+from .engine import Engine
 
 
 @decorators.api_view(['GET'])
 def search(request):
-    return Response('test')
+    engine = Engine()
+    return engine.test()
