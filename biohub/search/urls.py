5ac4dfdcb3bb517f8ff6f963502ebfecaebb2000
Jiyan He <volltin@live.com>
2018-10-16 15:56:09 +0800

    Add search

--- biohub/search/urls.py
+++ biohub/search/urls.py
@@ -1,13 +1,8 @@
 from biohub.core.routes import register_api, url
 from rest_framework.routers import DefaultRouter
 
-from .views import search
+from . import views
 
-router = DefaultRouter()
-
-router.register(r'^search/', search, base_name='search')
-
-
-register_api('', [
-
-] + router.urls, 'search')
+register_api(r'^', [
+    url(r'^search/', views.search, name='search')
+], 'search')
--- biohub/search/views.py
+++ biohub/search/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import viewsets, mixins, decorators
 # Create your views here.
 
+
 @decorators.api_view(['GET'])
 def search(request):
     return Response('test')
