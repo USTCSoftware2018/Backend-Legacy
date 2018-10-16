b595cb41942b0f3339794fc15f6beab04d382e78
_rika <i@ksqsf.moe>
2018-10-17 04:36:40 +0800

    fix signal delete comment

--- biohub/community/signals.py
+++ biohub/community/signals.py
@@ -84,7 +84,7 @@ def send_notice_to_commented_report_author(instance, raw, created, using, update
 
 @receiver(pre_delete, sender=Comment)
 def remove_notices_on_delete_comment(instance, using, **kwargs):
-    report = instance.to_comment
+    report = instance.to_report
     Notice.objects.filter(
         user=report.author,
         actor=instance.user,
