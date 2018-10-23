40dd144494e03af2a36433d1a09e04ab970d9442
_rika <i@ksqsf.moe>
2018-10-23 15:03:57 -0400

    search: fix regexp

--- biohub/search/utils.py
+++ biohub/search/utils.py
@@ -6,6 +6,6 @@ punct_ws_re = r'[\s{}]+'.format(punctuation + whitespace).replace('-', r'\-')
 
 
 def split_punct(x: str):
-    r = re.compile(r'[\s{}]+'.format(punct_ws_re))
+    r = re.compile(punct_ws_re)
     return r.split(x)
 
